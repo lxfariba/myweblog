e1da88c899a1ea4ea483da772027dd152aeca63b
lxfariba <f.karvand@gmail.com>
2016-06-19 23:07:41 +0430

    ADDED myweblog

--- weblog/views.py
+++ weblog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from weblog.models import Content
 
-# Create your views here.
+def home(request):
+	contents = Content.objects.all()
+	return render(request,'weblog/index.html',{'contents':contents})
