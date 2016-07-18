0b91c9144dba066e5574e3a30688cd46576df074
MahdiXareie <Mahdi.elf@gmail.com>
2016-07-18 22:07:39 +0430

    admin model class has been created for Content

--- weblog/admin.py
+++ weblog/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Content
 
 
-admin.site.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    change_form_template = 'weblog/admin/change_form.html'
 
+
+admin.site.register(Content, ContentAdmin)
